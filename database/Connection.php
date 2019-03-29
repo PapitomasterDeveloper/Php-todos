98997ce4ef7adef6696ee1400bb84f9567d702c8
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-03-29 13:50:52 -0600

    Refactoring work

--- database/Connection.php
+++ database/Connection.php
@@ -1,22 +1,32 @@
 <?php
 
 class Connection
+
 {
         // Static = something that is going to help anyone without any instance work
         public static function make()
         {
-                try
-        {
-                return new PDO('mysql:host=127.0.0.1;dbname=mytodo','root',' ');
-        }
-        catch(PDOException $e)
-        {
+
+            try
+            {
+
+              return new PDO('mysql:host=127.0.0.1;dbname=mytodo','root',' ');
+
+            }
+
+            catch(PDOException $e)
+
+            {
+
                 echo($e->getMessage());
                 die('<br/> Could not connect');
+
         }
 
         }
+
 }
+
 ?>
 
 
--- index.php
+++ index.php
@@ -1,33 +1,11 @@
 <?php 
 
-require 'functions.php';
+//require 'functions.php';
 require 'Task.php';
-require 'database/Connection.php';
-// try
-// 	{
-// 		$pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo','root',' ');
-// 	}
-// 	catch(PDOException $e)
-// 	{
-// 		echo($e->getMessage());
-// 		die('<br/> Could not connect');
-// 	}
 
-// $statement = $pdo->prepare('select * from todos');
+$query = require 'bootstrap.php';
 
-// 	$statement->execute();
-
-// 	// Fetching results in an array fashion
-// 	// dd($statement->fetchAll());
-
-// 	// Fetching results in an object fashion
-// 	// $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
-// 	$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-
-// Using the Connection class with his static method make in a static fashion
-$pdo = Connection::make();
-
-$tasks = fetchAllTasks($pdo);
+$tasks = $query->selectAll('todos');
 
 require 'index.view.php';
 
