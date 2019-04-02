2601a4ff4ca58c86c08576acc1c87ba4f7c8204f
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-04-01 18:00:33 -0600

    Created the basic MVC structure with the convention name for each file, also with his own folder, created the routes for each rendered view

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php 
 
 //require 'functions.php';
-require 'Task.php';
+//require 'Task.php';
 
-$query = require 'bootstrap.php';
-
-$tasks = $query->selectAll('todos');
-
-require 'index.view.php';
+$database = require 'bootstrap.php';
 
 ?>
